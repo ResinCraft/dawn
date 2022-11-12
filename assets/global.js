bb9ae77757490704f5bc7aad83dee89b39371370
Jono Hall <jonohall@gmail.com>
2022-11-12 15:23:33 +1300

    Initial Commit

--- assets/global.js
+++ assets/global.js
@@ -757,7 +757,80 @@ class VariantSelects extends HTMLElement {
   constructor() {
     super();
     this.addEventListener('change', this.onVariantChange);
+
+    /* *** Dynamic Selectors - Part 1 - Start *** */
+    this.rebuildOptions();
+    /* *** Dynamic Selectors - Part 1 - End *** */
+
   }
+
+  /* *** Dynamic Selectors - Part 2 - Start *** */
+  rebuildOptions() {
+    //get the option sets (option1, option2 etc)
+    const fieldsets = document.querySelectorAll('fieldset.product-form__input');
+
+    //build an array of currently selected options
+    const selectedOptions = [];
+    fieldsets.forEach((fieldset, i) => {
+        selectedOptions[i] = fieldsets[i].querySelector('input:checked').value;
+    });
+
+    //loop through the option sets remove any invalid options
+    fieldsets.forEach((fieldset, i) => {
+        if(i > 0) {
+            const inputs = fieldset.querySelectorAll('input');
+            inputs.forEach(input => {
+                //get the label for the current input and hide it if it is not a valid combo option
+                const label = fieldset.querySelector(`label[for="${input.id}"]`);
+                if(this.validCombo(input.value,i,selectedOptions) == false) {
+                    label.style.display = "none";
+                } else {
+                    label.style.display = "";
+                }
+                
+            });
+        }
+    });
+
+    //if the default selected option happens to be removed with the function above, select the first available option instead
+    for (var i = 0, count = fieldsets.length, change = false; i < count && !change; i++) {
+        const fieldset = fieldsets[i];
+        if(i > 0) {
+            const selectedOption = fieldset.querySelector('input:checked');
+            const selectedLabel = fieldset.querySelector(`label[for="${selectedOption.id}"]`);
+            if(selectedLabel.style.display == "none") {
+                const firstValidLabel = fieldset.querySelector(`label:not([style*="display: none"])`);
+                const firstValidInput = document.getElementById(firstValidLabel.getAttribute("for"));
+                firstValidInput.checked = true;
+
+                //if an option has been changed, break out of the loop and restart the whole process with the newly selected option
+                change = true;
+                this.onVariantChange();
+            }
+        }
+    }
+}
+
+validCombo(inputValue,optionLevel,selectedOptions) {
+    const productJson = JSON.parse(this.querySelector('[type="application/json"]').textContent);
+    let validCombo = new Boolean(false);
+
+    if(optionLevel == 1) {
+        productJson.map(function(v) {
+            if(v.option1 == selectedOptions[0] && v.option2 == inputValue) {
+                validCombo = true;
+            }
+        });
+    } else {
+        productJson.map(function(v) {
+            if(v.option1 == selectedOptions[0] && v.option2 == selectedOptions[1] && v.option3 == inputValue) {
+                validCombo = true;
+            }
+        });
+    }
+    return validCombo;
+}
+/* *** Dynamic Selectors - Part 2 - End *** */
 
   onVariantChange() {
     this.updateOptions();
@@ -777,6 +850,10 @@ class VariantSelects extends HTMLElement {
       this.renderProductInfo();
       this.updateShareUrl();
     }
+
+    /* *** Dynamic Selectors - Part 3 - Start *** */
+    this.rebuildOptions();
+    /* *** Dynamic Selectors - Part 3 - End *** */
   }
 
   updateOptions() {
